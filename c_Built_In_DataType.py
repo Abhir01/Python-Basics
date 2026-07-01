608fe3d2a4fc75c5a742c4a4a371694ef58b0006
Abhishek Rawat <abhishekrawat@Abhisheks-MacBook-Air.local>
2026-07-01 16:54:42 +0530

    Updated at July 1, 4.54 PM

---
+++ c_Built_In_DataType.py
@@ -0,0 +1,21 @@
+# LIST 
+marks = [10,20,90,40]
+# print(marks)
+# print(marks[-4])
+
+# print(marks.sort())
+# print(ma
+#rks.reverse())
+# marks.sort()
+# marks.reverse()
+# marks.sort(reverse=True)
+# print(marks)
+
+x = 90
+idx = 0
+for i in marks:
+    if(i == x):
+        print(idx)
+        break
+    idx = idx+1
+ 
