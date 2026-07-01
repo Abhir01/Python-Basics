a5e7a82d1bfc564dad9a8c4fb8f96756dc530819
Abhishek Rawat <abhishekrawat@Abhisheks-MacBook-Air.local>
2026-07-01 16:57:08 +0530

    added at 1 July - 4.56 PM

---
+++ d_Built_In_DataType2.py
@@ -0,0 +1,18 @@
+# dict = {
+#     "name": "abhi",
+#     "class": "10th"
+# }
+# print(dict["name"])
+# # print(dict["hero"])
+# print(dict.get("name"))
+# print(dict.get("her"))
+
+# sets
+s = {1,2,3,4}
+# s[0] = '5'
+print(s)
+
+s1 = {3,4,5,7}
+a = s.union(s1)
+print(a)
+
